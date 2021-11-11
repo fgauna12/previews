69827c8a4dbb02b37b860a06332fc448d07080fb
Facundo Gauna <fgauna12@gmail.com>
2021-11-11 15:38:19 -0800

    Tweaks

--- create-preview.sh
+++ create-preview.sh
@@ -5,7 +5,7 @@ export PR_ID="$2"
 export APP_ID="pr-$REPO-$PR_ID"
 export FULLY_QUALIFIED_IMAGE="$3"
 
-cat <<EOF | tee $PWD/apps/$APP_ID.yaml
+cat <<EOF | tee $PWD/apps/templates/$APP_ID.yaml
 apiVersion: argoproj.io/v1alpha1
 kind: Application
 metadata:
@@ -16,7 +16,7 @@ metadata:
 spec:
   project: previews
   source:
-    path: helm
+    path: k8s
     repoURL: https://github.com/fgauna12/$REPO.git
     targetRevision: HEAD
     kustomize:
