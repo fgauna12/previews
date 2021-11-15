19eb1f6e25d2061ba3abf15acca2a35cc39cfc29
Facundo Gauna <fgauna12@gmail.com>
2021-11-15 15:38:20 -0800

    Tweaked the environment deletion script

--- delete-preview.sh
+++ delete-preview.sh
@@ -4,5 +4,6 @@ export REPO="$1"
 export PR_ID="$2"
 export APP_ID="pr-$REPO-$PR_ID"
 
-echo "Removing preview environment"
-rm $PWD/apps/$APP_ID.yaml
+ENVIRONMENT_FILE="$PWD/apps/$APP_ID.yaml"
+echo "Removing preview environment $ENVIRONMENT_FILE"
+rm $ENVIRONMENT_FILE
