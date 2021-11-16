bb02aeb02c9f33fa0274c5aae5c10331442a29f6
Facundo Gauna <fgauna12@gmail.com>
2021-11-15 16:43:32 -0800

    Removing the ingress file too

--- delete-preview.sh
+++ delete-preview.sh
@@ -5,5 +5,7 @@ export PR_ID="$2"
 export APP_ID="pr-$REPO-$PR_ID"
 
 ENVIRONMENT_FILE="$PWD/apps/$APP_ID.yaml"
+INGRESS_FILE="$PWD/apps/$APP_ID-ingress.yaml"
 echo "Removing preview environment $ENVIRONMENT_FILE"
 rm $ENVIRONMENT_FILE
+rm $INGRESS_FILE
