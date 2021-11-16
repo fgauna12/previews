a4fd5856d78a642dd550e36c8bdf40fe8ccd022b
Facundo Gauna <fgauna12@gmail.com>
2021-11-15 16:20:43 -0800

    wip - testing addition of custom ingress

--- create-preview.sh
+++ create-preview.sh
@@ -5,6 +5,7 @@ export PR_ID="$2"
 export APP_ID="pr-$REPO-$PR_ID"
 export FULLY_QUALIFIED_IMAGE="$3"
 
+echo "Creating Argo application definition"
 cat <<EOF | tee $PWD/apps/$APP_ID.yaml
 apiVersion: argoproj.io/v1alpha1
 kind: Application
@@ -31,4 +32,34 @@ spec:
       prune: true
     syncOptions:
     - CreateNamespace=true
+EOF
+
+echo "Creating ingress for the preview environment"
+
+cat << EOF | tee $PWD/apps/$($APP_ID)-ingress.yaml
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: $APP_ID-ingress
+  namespace: $APP_ID
+  annotations:
+    cert-manager.io/cluster-issuer: letsencrypt
+    kubernetes.io/ingress.class: nginx
+    kubernetes.io/tls-acme: "true"
+spec:
+  rules:
+  - host: $APP_ID-socks-shop.azure.boxboat.io
+    http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service:
+            name: front-end
+            port:
+              number: 80
+  tls:
+  - hosts:
+    - $APP_ID-socks-shop.azure.boxboat.io
+    secretName: $APP_ID-socks-shop-tls-secret
 EOF
